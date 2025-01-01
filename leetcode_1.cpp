cca905bcfad7209a126612c9b550d1efc0a576ef
Thevenin1849 <sysy1849@gmail.com>
2025-01-01 18:21:00 +0900

    way1

--- leetcode_1.cpp
+++ leetcode_1.cpp
@@ -5,10 +5,7 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) { 
                 if (nums[i] + nums[j] == target) {
-                    vector<int> result;
-                    result.push_back(i); 
-                    result.push_back(j);
-                    return result;
+                    return {i,j};
                 }
             }
         }
@@ -51,3 +48,7 @@ public:
         return {i, j}; // 결과 반환
     }
 };
+
+//HashMap
+
+
