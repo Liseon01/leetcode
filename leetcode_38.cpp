8624d4d543011831f4efc51a4d9e4361a7b3d0bc
Thevenin1849 <sysy1849@gmail.com>
2025-02-06 15:52:06 +0900

    Count and Say

--- leetcode_38.cpp
+++ leetcode_38.cpp
@@ -1,3 +1,4 @@
+// my solution
 class Solution {
 public:
     string countAndSay(int n) {
@@ -24,3 +25,40 @@ private:
         return res;
     }
 };
+
+// optimal solution
+
+class Solution {
+public:
+    string countAndSay(int n) {
+        string result = "1";
+        
+        for (int i = 1; i < n; ++i) {
+            string temp;
+            // 현재 result의 길이를 고려하여 최대 2배 길이로 예약
+            temp.reserve(result.size() * 2);
+            int len = result.size();
+            int j = 0;
+            
+            while (j < len) {
+                char currentChar = result[j];
+                int count = 0;
+                // 같은 숫자가 연속되는 개수 세기
+                while (j < len && result[j] == currentChar) {
+                    ++count;
+                    ++j;
+                }
+                // count가 한 자릿수임을 가정하여 문자로 변환
+                temp.push_back('0' + count);
+                temp.push_back(currentChar);
+            }
+            
+            // 새로운 문자열이 result가 됨 (move로 불필요한 복사 줄이기)
+            result = move(temp);
+        }
+        
+        return result;
+    }
+};
+
+
